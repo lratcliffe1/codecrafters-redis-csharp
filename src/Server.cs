88d24b31c8f78c27c64d45aea295c72802819a15
Liam Ratcliffe <liam.ratcliffe@btinternet.com>
2026-01-30 09:33:22 +0100

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -1,6 +1,14 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-server.AcceptSocket(); // wait for client
+
+while (true)
+{
+  Socket client = server.AcceptSocket();
+  client.Send(Encoding.UTF8.GetBytes("+PONG\r\n"));
+  client.Shutdown(SocketShutdown.Both);
+  client.Close();
+}
