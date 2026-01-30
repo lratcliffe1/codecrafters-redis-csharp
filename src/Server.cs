3a92955c75ff5ca6328a2282b3a4e6fde5526b37
Liam Ratcliffe <liam.ratcliffe@btinternet.com>
2026-01-30 10:23:07 +0100

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -2,13 +2,40 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-TcpListener server = new TcpListener(IPAddress.Any, 6379);
-server.Start();
+TcpListener? server = null;
 
-while (true)
+try
 {
-  Socket client = server.AcceptSocket();
-  client.Send(Encoding.UTF8.GetBytes("+PONG\r\n"));
-  client.Shutdown(SocketShutdown.Both);
-  client.Close();
+  server = new TcpListener(IPAddress.Any, 6379);
+  server.Start();
+
+  byte[] bytes = new byte[256];
+  string? data = null;
+
+  while (true)
+  {
+    using TcpClient client = server.AcceptTcpClient();
+
+    data = null;
+
+    NetworkStream stream = client.GetStream();
+
+    int i;
+    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+    {
+      data = Encoding.ASCII.GetString(bytes, 0, i);
+
+      byte[] msg = Encoding.UTF8.GetBytes("+PONG\r\n");
+      stream.Write(msg, 0, msg.Length);
+      stream.Flush();
+    }
+  }
+}
+catch (SocketException e)
+{
+  Console.WriteLine("SocketException: {0}", e);
+}
+finally
+{
+  server?.Stop();
 }
