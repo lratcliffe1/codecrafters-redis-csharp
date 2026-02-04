886da96b66bbdcc0930c1654a9d1a7087fe5b977
Liam Ratcliffe <liam.ratcliffe@btinternet.com>
2026-02-04 14:36:34 +0100

    codecrafters submit [skip ci]

--- src/Commands/XAddCommand.cs
+++ src/Commands/XAddCommand.cs
@@ -8,101 +8,92 @@ public static class XAddCommand
 {
   public static string Process(List<RespValue> args)
   {
-    if (!HasValidArity(args))
+    if (args.Count < 5 || args.Count % 2 != 1)
     {
       return CommandHepler.BuildError("wrong number of arguments for 'xadd'");
     }
 
-    if (!TryReadKeyAndId(args, out var key, out var id, out var error))
+    string? key = CommandHepler.ReadBulkOrSimple(args[1]);
+
+    if (string.IsNullOrEmpty(key))
     {
-      return error;
+      return CommandHepler.BuildError("invalid of key for 'xadd'");
     }
 
-    if (!ValidateNewEntryId(key, id, out error))
+    string? id = CommandHepler.ReadBulkOrSimple(args[2]);
+
+    if (string.IsNullOrEmpty(id))
     {
-      return error;
+      return CommandHepler.BuildError("invalid of key for 'xadd'");
     }
 
-    var keyValuePairs = ReadKeyValuePairs(args);
+    if (id == "0-0")
+    {
+      return CommandHepler.BuildError("The ID specified in XADD must be greater than 0-0");
+    }
+
+    var testing = id.Split("-");
+
+    if (Cache.TryGetValue(key, out var cacheValue) && cacheValue != null)
+    {
+      if (cacheValue.TryGetStream(out var oldValue))
+      {
+        var lastValue = oldValue.Last();
+
+        var lastValueIdSplit = lastValue.Id.Split("-").Select(long.Parse).ToList();
+        var a = Parse(testing[0]);
+        var b = Parse(testing[1], a, lastValueIdSplit);
+
+        var newValueIdSplit = new List<long> { a, b };
+        id = string.Join("-", newValueIdSplit);
+
+        if (lastValueIdSplit[0] > newValueIdSplit[0])
+          return CommandHepler.BuildError("The ID specified in XADD is equal or smaller than the target stream top item");
+
+        if (lastValueIdSplit[0] == newValueIdSplit[0] && lastValueIdSplit[1] >= newValueIdSplit[1])
+          return CommandHepler.BuildError("The ID specified in XADD is equal or smaller than the target stream top item");
+      }
+    }
+    else if (testing[1] == "*")
+    {
+      if (testing[0] == "0")
+        id = string.Join("-", testing[0], 1);
+      else
+        id = string.Join("-", testing[0], 0);
+    }
+
+    Dictionary<string, string> keyValuePairs = [];
+
+    for (var i = 1; i < args.Count; i += 2)
+    {
+      keyValuePairs.Add(CommandHepler.ReadBulkOrSimple(args[i])!, CommandHepler.ReadBulkOrSimple(args[i + 1])!);
+    }
+
     Cache.Set(key, CacheValue.Stream([new StreamEntry(id, keyValuePairs)]));
 
     return CommandHepler.FormatBulk(id);
   }
 
-  private static bool HasValidArity(List<RespValue> args)
+  private static long Parse(string value)
   {
-    return args.Count >= 5 && args.Count % 2 == 1;
+    if (value == "*")
+    {
+      return DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+    }
+
+    return long.Parse(value);
   }
 
-  private static bool TryReadKeyAndId(List<RespValue> args, out string key, out string id, out string error)
+  private static long Parse(string value, long previousValue, List<long> lastValueIdSplit)
   {
-    key = CommandHepler.ReadBulkOrSimple(args[1]) ?? string.Empty;
-    if (string.IsNullOrEmpty(key))
+    if (value == "*")
     {
-      error = CommandHepler.BuildError("invalid of key for 'xadd'");
-      id = string.Empty;
-      return false;
+      if (previousValue > lastValueIdSplit[0])
+        return 0;
+
+      return lastValueIdSplit[1] + 1;
     }
 
-    id = CommandHepler.ReadBulkOrSimple(args[2]) ?? string.Empty;
-    if (string.IsNullOrEmpty(id))
-    {
-      error = CommandHepler.BuildError("invalid of key for 'xadd'");
-      return false;
-    }
-
-    if (id == "0-0")
-    {
-      error = CommandHepler.BuildError("The ID specified in XADD must be greater than 0-0");
-      return false;
-    }
-
-    error = string.Empty;
-    return true;
-  }
-
-  private static bool ValidateNewEntryId(string key, string id, out string error)
-  {
-    error = string.Empty;
-    if (!Cache.TryGetValue(key, out var cacheValue) || cacheValue == null)
-    {
-      return true;
-    }
-
-    if (!cacheValue.TryGetStream(out var oldValue) || oldValue.Count == 0)
-    {
-      return true;
-    }
-
-    var lastValue = oldValue.Last();
-    var lastValueIdSplit = lastValue.Id.Split("-").Select(int.Parse).ToList();
-    var newValueIdSplit = id.Split("-").Select(int.Parse).ToList();
-
-    if (lastValueIdSplit[0] > newValueIdSplit[0])
-    {
-      error = CommandHepler.BuildError("The ID specified in XADD is equal or smaller than the target stream top item");
-      return false;
-    }
-
-    if (lastValueIdSplit[0] == newValueIdSplit[0] && lastValueIdSplit[1] >= newValueIdSplit[1])
-    {
-      error = CommandHepler.BuildError("The ID specified in XADD is equal or smaller than the target stream top item");
-      return false;
-    }
-
-    return true;
-  }
-
-  private static Dictionary<string, string> ReadKeyValuePairs(List<RespValue> args)
-  {
-    Dictionary<string, string> keyValuePairs = [];
-    for (var i = 3; i < args.Count; i += 2)
-    {
-      var field = CommandHepler.ReadBulkOrSimple(args[i]) ?? string.Empty;
-      var value = CommandHepler.ReadBulkOrSimple(args[i + 1]) ?? string.Empty;
-      keyValuePairs.Add(field, value);
-    }
-
-    return keyValuePairs;
+    return long.Parse(value);
   }
 }
